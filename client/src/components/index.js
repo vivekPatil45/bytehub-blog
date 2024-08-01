ae73cc7ea7f26e95cb65ccf9a186c92ee4dbe5c0
Vivek Patil <vivekrp4503@gmail.com>
2024-08-01 18:42:43 +0530

    complete dark mode functionality

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,9 +1,11 @@
 import Header from "./Header";
 import FooterComp from "./Footer";
 import OAuth from "./OAuth";
+import ThemeProvider from "./ThemeProvider";
 
 export{
     Header,
     FooterComp,
-    OAuth
+    OAuth,
+    ThemeProvider
 };
