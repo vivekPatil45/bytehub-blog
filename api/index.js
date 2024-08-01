5c91b79a22544802865ca3b3a8b0d87635094fc4
Vivek Patil <vivekrp4503@gmail.com>
2024-08-01 15:40:55 +0530

    connect to the database

--- api/index.js
+++ api/index.js
@@ -2,6 +2,22 @@ import express from 'express';
 
 const app = express();
 
+
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose
+    .connect(process.env.MONGO)
+    .then(() => {
+        console.log('MongoDb is connected');
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000!');
 });
