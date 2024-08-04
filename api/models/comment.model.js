0d9c30739d52d6f922a19cd8f413a1bfac212020
Vivek Patil <vivekrp4503@gmail.com>
2024-08-04 23:41:15 +0530

    add like functionality to the comment component

--- api/models/comment.model.js
+++ api/models/comment.model.js
@@ -3,24 +3,24 @@ import mongoose from 'mongoose';
 const commentSchema = new mongoose.Schema(
     {
         content: {
-        type: String,
-        required: true,
+            type: String,
+            required: true,
         },
         postId: {
-        type: String,
-        required: true,
+            type: String,
+            required: true,
         },
         userId: {
-        type: String,
-        required: true,
+            type: String,
+            required: true,
         },
         likes: {
-        type: Array,
-        default: [],
+            type: Array,
+            default: [],
         },
         numberOfLikes: {
-        type: Number,
-        default: 0,
+            type: Number,
+            default: 0,
         },
     },
     { timestamps: true }
--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -1,11 +1,18 @@
 import moment from 'moment';
 import { useEffect, useState } from 'react';
+import { FaThumbsUp } from 'react-icons/fa';
+import { useSelector } from 'react-redux';
+
 
 import React from 'react'
 
-function Comment({comment}) {
+function Comment({comment , onLike}) {
     const [user, setUser] = useState({});
-    console.log(user);
+    
+    const { currentUser } = useSelector((state) => state.user);
+    
+
+
     useEffect(() => {
         const getUser = async () => {
         try {
@@ -20,6 +27,7 @@ function Comment({comment}) {
         };
         getUser();
     }, [comment]);
+
     return (
         <div className='flex p-4 border-b dark:border-gray-600 text-sm'>
             <div className='flex-shrink-0 mr-3'>
@@ -34,9 +42,31 @@ function Comment({comment}) {
                     <span className='font-bold mr-1 text-xs truncate'>
                         {user ? `@${user.username}` : 'anonymous user'}
                     </span>
-                    <span className='text-gray-500 text-xs'>{moment(comment.createdAt).fromNow()}</span>
+                    <span className='text-gray-500 text-xs'>
+                        {moment(comment.createdAt).fromNow()}
+                    </span>
                 </div>
                 <p className='text-gray-500 pb-2'>{comment.content}</p>
+                <div className='flex items-center pt-2 text-xs border-t dark:border-gray-700 max-w-fit gap-2'>
+                    <button
+                        type='button'
+                        onClick={() => onLike(comment._id)}
+                        className={`text-gray-400 hover:text-blue-500 ${
+                            currentUser &&
+                            comment.likes.includes(currentUser._id) &&
+                            '!text-blue-500'
+                        }`}
+                    >
+                        <FaThumbsUp className='text-sm' />
+                    </button>
+                    <p className='text-gray-400'>
+                        {comment.numberOfLikes > 0 &&
+                            comment.numberOfLikes +
+                                ' ' +
+                            (comment.numberOfLikes === 1 ? 'like' : 'likes')
+                        }
+                    </p>
+                </div>
             </div>
         </div>
     );
